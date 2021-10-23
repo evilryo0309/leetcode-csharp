8fe823244861d5807ad0b05e1b4a35b75c922b9a
John HoHoHo <evilryo0309@diamondonline.com.tw>
2021-10-23 13:26:31 +0800

    Add Debug.Assert.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 /// <summary>
 /// 1. Two Sum
@@ -18,6 +20,9 @@ int[] TwoSum(int[] nums, int target)
 
     return query;
 }
+Debug.Assert(TwoSum(new int[] { 2, 7, 11, 15 }, 9).SequenceEqual(new int[] { 0, 1 }));
+Debug.Assert(TwoSum(new int[] { 3, 2, 4 }, 6).SequenceEqual(new int[] { 1, 2 }));
+Debug.Assert(TwoSum(new int[] { 3, 3 }, 6).SequenceEqual(new int[] { 0, 1 }));
 
 /// <summary>
 /// 2. Add Two Numbers
@@ -53,6 +58,39 @@ ListNode AddTwoNumbers(ListNode l1, ListNode l2)
 
     return answer;
 }
+Debug.Assert(new Func<bool>(() =>
+{
+    var answer = AddTwoNumbers(
+            new ListNode(2, new ListNode(4, new ListNode(3))),
+            new ListNode(5, new ListNode(6, new ListNode(4))));
+
+    return answer.val == 7 && answer.next.val == 0 && answer.next.next.val == 8;
+})());
+Debug.Assert(new Func<bool>(() =>
+{
+    var answer = AddTwoNumbers(
+            new ListNode(0),
+            new ListNode(0));
+
+    return answer.val == 0;
+})());
+Debug.Assert(new Func<bool>(() =>
+{
+    var answer = AddTwoNumbers(
+            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))))))),
+            new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))));
+
+    return
+    answer.val == 8 &&
+    answer.next.val == 9 &&
+    answer.next.next.val == 9 &&
+    answer.next.next.next.val == 9 &&
+    answer.next.next.next.next.val == 0 &&
+    answer.next.next.next.next.next.val == 0 &&
+    answer.next.next.next.next.next.next.val == 0 &&
+    answer.next.next.next.next.next.next.next.val == 1;
+
+})());
 
 /// <summary>
 /// 3. Longest Substring Without Repeating Characters
@@ -80,6 +118,10 @@ int LengthOfLongestSubstring(string s)
 
     return answer;
 }
+Debug.Assert(LengthOfLongestSubstring("abcabcbb") == 3);
+Debug.Assert(LengthOfLongestSubstring("bbbbb") == 1);
+Debug.Assert(LengthOfLongestSubstring("pwwkew") == 3);
+Debug.Assert(LengthOfLongestSubstring("") == 0);
 
 /// <summary>
 /// 4. Median of Two Sorted Arrays
@@ -104,6 +146,11 @@ double FindMedianSortedArrays(int[] nums1, int[] nums2)
 
     return answer;
 }
+Debug.Assert(FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }) == 2.00000f);
+Debug.Assert(FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }) == 2.50000f);
+Debug.Assert(FindMedianSortedArrays(new int[] { 0, 0 }, new int[] { 0, 0 }) == 0.00000f);
+Debug.Assert(FindMedianSortedArrays(new int[] { }, new int[] { 1 }) == 1.00000f);
+Debug.Assert(FindMedianSortedArrays(new int[] { 2 }, new int[] { }) == 2.00000f);
 
 /// <summary>
 /// 5. Longest Palindromic Substring
@@ -115,3 +162,8 @@ string LongestPalindrome(string s)
 
     return answer;
 }
+
+Debug.Assert("bab" == LongestPalindrome("babad"));
+Debug.Assert("bb" == LongestPalindrome("cbbd"));
+Debug.Assert("a" == LongestPalindrome("a"));
+Debug.Assert("a" == LongestPalindrome("ac"));
